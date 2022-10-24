ebcc225112c0999f195ec44aad46a4856887c00f
faelcjeronymo <rafaelcjeronymo@outlook.com>
2022-10-24 14:58:38 -0300

    continuar

--- public/js/components/header/header.js
+++ public/js/components/header/header.js
@@ -11,10 +11,16 @@ class Header extends HTMLElement {
                 <button class="navbar-toggler px-0" type="button" data-bs-toggle="collapse" data-bs-target="#sidebar" aria-controls="sidebar" aria-expanded="false" aria-label="Toggle Sidebar">
                     <i class="fa fa-bars"></i>
                 </button>
-                <div class="navbar-profile">
+                <div class="navbar-profile dropdown">
                     <span class="text-primary fw-semibold">Meu Perfil</span>
                     <img src="./public/img/imagem-rafael.png" alt="Imagem do usuário">
                     <i class="fa fa-chevron-down text-primary"></i>
+                    <button class="btn btn-transparent stretched-link" data-bs-toggle="dropdown" aria-expanded="false"></button>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
                 </div>
             </div>
         </nav>
